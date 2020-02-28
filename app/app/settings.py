37b320f5dc71003a2bf758b94e861358534d6e15
Blanchard, Lucas <lucas.blanchard@accenture.com>
2020-02-27 21:04:05 -0600

    rds

--- app/app/settings.py
+++ app/app/settings.py
@@ -87,10 +87,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ.get('DB_HOST'),
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
+        'HOST': 'database-1.cfaevwniko3z.us-east-1.rds.amazonaws.com',
+        'NAME': 'database-1',
+        'USER': 'postgres',
+        'PASSWORD': 'password', 
+        'PORT': 5432,
     }
 }
 
